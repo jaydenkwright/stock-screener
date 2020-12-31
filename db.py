26cb6884ba22ca6317d41af57cf3b0437f197049
Jayden Wright <jaydenkwright3@gmail.com>
2020-12-30 23:13:48 -0500

    test commit

--- db.py
+++ db.py
@@ -3,6 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from config import DATABASE_URL
 
+
 engine = create_engine(
     DATABASE_URL, echo=True
 )

cf99f003ffa097ba1bda9e45d40972ec0819a77d
Jayden Wright <jaydenkwright3@gmail.com>
2020-12-31 23:43:26 -0500

    Update stock model

--- stockModel.py
+++ stockModel.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 from sqlalchemy import Boolean, Column, String, Integer, DateTime
 from sqlalchemy.orm import sessionmaker
-from db import session, Base
+from db import session, Base, engine
 
 class Stock(Base):
     __tablename__ = 'stocks'
@@ -9,15 +9,23 @@ class Stock(Base):
     name = Column('name', String, nullable=False)
     symbol = Column('symbol', String, nullable=False, unique=True)
     exchange = Column('exchange', String, nullable=False)
+    sector = Column('sector', String, nullable=False)
+    industry = Column('industry', String, nullable=False)
+    location = Column('location', String, nullable=False)
+    founded = Column('founded', Integer, nullable=False)
     date = Column(DateTime, default=datetime.utcnow)
 
-    def __init__(self, name, symbol, exchange):
+    def __init__(self, name, symbol, exchange, sector, industry, location, founded):
         self.name = name
         self.symbol = symbol
         self.exchange = exchange
+        self.sector = sector
+        self.industry = industry
+        self.location = location
+        self.founded = founded
 
 
 
 
-#Base.metadata.create_all(bind=engine)
+# Base.metadata.create_all(bind=engine)
 

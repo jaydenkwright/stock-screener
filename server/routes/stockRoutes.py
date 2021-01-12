3620ad4c4cd58285d1a3539cb5e9658a6d0488e2
Jayden Wright <jaydenkwright3@gmail.com>
2021-01-11 23:25:20 -0500

    Added routes to find specific stocks

--- server/routes/stockRoutes.py
+++ server/routes/stockRoutes.py
@@ -1,8 +1,14 @@
-from fastapi import Request
+from fastapi import Request, BackgroundTasks
 from server import app
 from server.models.stockModel import Stock
+from server.models.stockInfoModel import StockInfo
+from server.models.priceModel import Price
 from server.db import session
-
+from sqlalchemy import func, desc
+import yfinance as yf
+import alpaca_trade_api as tradeapi
+from server.config import ALPACA_API_KEY, ALPACA_API_SECRET, ALPACA_BASE_URL
+from datetime import datetime
 
 @app.get('/api/stocks/all')
 def getAllStocks():
@@ -17,11 +23,39 @@ def getStock(id: int):
     except Exception as error:
         return error
 
+def updateInfo(id: int):
+    symbol = session.query(Stock).get(id).symbol
+    stockInfo = yf.Ticker(symbol).info
+    try:
+        marketCap = stockInfo['marketCap']
+        volume = stockInfo['volume']
+        twoHundredDayAverage = stockInfo['twoHundredDayAverage']
+        fiftyDayAverage = stockInfo['fiftyDayAverage']
+        forwardPe = stockInfo['forwardPE']
+        forwardEps = stockInfo['forwardEps']
+        dividendYield = stockInfo['dividendYield'] * 100 if stockInfo['dividendYield'] else None
+        info = session.query(StockInfo).filter(StockInfo.stockId == id)
+
+        if info:
+            info.update({'marketCap': marketCap, 'volume': volume, 'twoHundredDayAverage': twoHundredDayAverage, 'fiftyDayAverage': fiftyDayAverage, 'forwardPe': forwardPe, 'forwardEps': forwardEps, 'dividendYield': dividendYield, 'dateUpdated': datetime.utcnow()}, synchronize_session="fetch")
+            session.commit()
+        session.close()
+    except Exception as error:
+        print(error)
+
+@app.get('/api/stock/info/{id}')
+def getStockInfo(id: int, background_task: BackgroundTasks):
+    try:
+        stockInfo = session.query(StockInfo).filter(StockInfo.stockId == id).first()
+        background_task.add_task(updateInfo, id)
+        return stockInfo
+    except Exception as error:
+        print(error)
+
 @app.get('/api/stock/symbol/{symbol}')
 def getStockBySymbol(symbol: str):
     try:
-        symbol = symbol.upper()
-        stock = session.query(Stock).filter(Stock.symbol == symbol).first()
+        stock = session.query(Stock).filter(func.lower(Stock.symbol) == func.lower(symbol)).first()
         return stock
     except Exception as error:
         return error
@@ -29,10 +63,26 @@ def getStockBySymbol(symbol: str):
 @app.get('/api/stocks/exchange/{exchange}')
 def getStocksByExchange(exchange: str):
     try:
-        exchange = exchange.upper()
-        stocks = session.query(Stock).filter(Stock.exchange == exchange).all()
+        stocks = session.query(Stock).filter(func.lower(Stock.exchange) == func.lower(exchange)).all()
         return stocks
     except Exception as error:
         print(error)
+
+@app.get('/api/stocks/sector/{sector}')
+def getStocksBySector(sector: str):
+    try:
+        stocks = session.query(Stock).filter(func.lower(Stock.sector) == func.lower(sector)).all()
+        return stocks
+    except Exception as error:
+        print(error)
+
+@app.get('/api/stocks/industry/{industry}')
+def getStocksByIndustry(industry: str):
+    try:
+        stocks = session.query(Stock).filter(func.lower(Stock.industry) == func.lower(industry)).all()
+        return stocks
+    except Exception as error:
+        print(error)
+
 
     

1e09009a73065d1801464c5bcea7f082b7260f48
Jayden Wright <jaydenkwright3@gmail.com>
2021-01-16 17:01:39 -0500

    Updated routes to return stockInfo

--- server/routes/stockRoutes.py
+++ server/routes/stockRoutes.py
@@ -3,6 +3,7 @@ from server import app
 from server.models.stockModel import Stock
 from server.models.stockInfoModel import StockInfo
 from server.models.priceModel import Price
+from server.tasks.stockTasks import updateInfo, updatePrices
 from server.db import session
 from sqlalchemy import func, desc
 import yfinance as yf
@@ -10,43 +11,11 @@ import alpaca_trade_api as tradeapi
 from server.config import ALPACA_API_KEY, ALPACA_API_SECRET, ALPACA_BASE_URL
 from datetime import datetime
 
-def updateInfo(id: int):
-    symbol = session.query(Stock).get(id).symbol
-    stockInfo = yf.Ticker(symbol).info
-    try:
-        marketCap = stockInfo['marketCap']
-        volume = stockInfo['volume']
-        twoHundredDayAverage = stockInfo['twoHundredDayAverage']
-        fiftyDayAverage = stockInfo['fiftyDayAverage']
-        forwardPe = stockInfo['forwardPE']
-        forwardEps = stockInfo['forwardEps']
-        dividendYield = stockInfo['dividendYield'] * 100 if stockInfo['dividendYield'] else None
-        info = session.query(StockInfo).filter(StockInfo.stockId == id)
-
-        if info:
-            info.update({'marketCap': marketCap, 'volume': volume, 'twoHundredDayAverage': twoHundredDayAverage, 'fiftyDayAverage': fiftyDayAverage, 'forwardPe': forwardPe, 'forwardEps': forwardEps, 'dividendYield': dividendYield, 'dateUpdated': datetime.utcnow()}, synchronize_session="fetch")
-            session.commit()
-        session.close()
-    except Exception as error:
-        print(error)
-
-def updatePrices(id: int):
-    api = tradeapi.REST(ALPACA_API_KEY, ALPACA_API_SECRET, base_url=ALPACA_BASE_URL)
-    symbol = session.query(Stock).get(id).symbol
-    barsets = api.get_barset(symbol, '1D', limit=31)
-    for bar in barsets[symbol]:
-        priceExist = session.query(Price).filter(Price.date == bar.t.date()).first()
-        if not priceExist:
-            price = Price(id, bar.o, bar.h, bar.l, bar.c, bar.t.date())
-            session.add(price)
-            session.commit()
-    session.close()
-
 @app.get('/api/stocks/all')
 def getAllStocks():
     try:
-        result = []
         stocks = session.query(Stock).all()
+        result = []
         for stock in stocks:
             item = {}
             item['stock'] = stock
@@ -71,10 +40,16 @@ def getStock(id: int, background_task: BackgroundTasks):
         return error
 
 @app.get('/api/stock/symbol/{symbol}')
-def getStockBySymbol(symbol: str):
+def getStockBySymbol(symbol: str, background_task: BackgroundTasks):
     try:
         stock = session.query(Stock).filter(func.lower(Stock.symbol) == func.lower(symbol)).first()
-        return stock
+        item = {}
+        item['stock'] = stock
+        item['stockInfo'] = session.query(StockInfo).filter(StockInfo.stockId == stock.id).first()
+        item['prices'] = session.query(Price).filter(Price.stockId == stock.id).order_by(desc(Price.date)).limit(31).all()
+        background_task.add_task(updateInfo, stock.id)
+        background_task.add_task(updatePrices, stock.id)
+        return item
     except Exception as error:
         return error
 
@@ -82,7 +57,13 @@ def getStockBySymbol(symbol: str):
 def getStocksByExchange(exchange: str):
     try:
         stocks = session.query(Stock).filter(func.lower(Stock.exchange) == func.lower(exchange)).all()
-        return stocks
+        result = []
+        for stock in stocks:
+            item = {}
+            item['stock'] = stock
+            item['stockInfo'] = session.query(StockInfo).filter(StockInfo.stockId == stock.id).first()
+            result.append(item)
+        return result
     except Exception as error:
         print(error)
 
@@ -90,7 +71,13 @@ def getStocksByExchange(exchange: str):
 def getStocksBySector(sector: str):
     try:
         stocks = session.query(Stock).filter(func.lower(Stock.sector) == func.lower(sector)).all()
-        return stocks
+        result = []
+        for stock in stocks:
+            item = {}
+            item['stock'] = stock
+            item['stockInfo'] = session.query(StockInfo).filter(StockInfo.stockId == stock.id).first()
+            result.append(item)
+        return result
     except Exception as error:
         print(error)
 
@@ -98,14 +85,12 @@ def getStocksBySector(sector: str):
 def getStocksByIndustry(industry: str):
     try:
         stocks = session.query(Stock).filter(func.lower(Stock.industry) == func.lower(industry)).all()
-        return stocks
+        result = []
+        for stock in stocks:
+            item = {}
+            item['stock'] = stock
+            item['stockInfo'] = session.query(StockInfo).filter(StockInfo.stockId == stock.id).first()
+            result.append(item)
+        return result
     except Exception as error:
         print(error)
-
-
-@app.get('/api/prices/{id}')
-def getPrices(id: int, background_task: BackgroundTasks):
-    prices = session.query(Price).filter(Price.stockId == id).order_by(desc(Price.date)).limit(31).all()
-    background_task.add_task(updatePrices, id)
-    return prices
-    

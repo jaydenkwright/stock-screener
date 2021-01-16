794d5d6928af555cc81413a5dc3a22f2da754d2e
Jayden Wright <jaydenkwright3@gmail.com>
2021-01-16 17:27:59 -0500

    Added HTTPException to handle errors

--- server/routes/stockRoutes.py
+++ server/routes/stockRoutes.py
@@ -1,4 +1,4 @@
-from fastapi import Request, BackgroundTasks
+from fastapi import Request, BackgroundTasks, HTTPException
 from server import app
 from server.models.stockModel import Stock
 from server.models.stockInfoModel import StockInfo
@@ -15,82 +15,100 @@ from datetime import datetime
 def getAllStocks():
     try:
         stocks = session.query(Stock).all()
-        result = []
-        for stock in stocks:
-            item = {}
-            item['stock'] = stock
-            item['stockInfo'] = session.query(StockInfo).filter(StockInfo.stockId == stock.id).first()
-            result.append(item)
-        return result
+        if stocks:
+            result = []
+            for stock in stocks:
+                item = {}
+                item['stock'] = stock
+                item['stockInfo'] = session.query(StockInfo).filter(StockInfo.stockId == stock.id).first()
+                result.append(item)
+            return result
+        else:
+            raise HTTPException(status_code=404, detail="No stocks were found!")
     except Exception as error:
-        print(error)
+        raise HTTPException(status_code=500, detail="Something went wrong!")
 
 @app.get('/api/stock/{id}')
 def getStock(id: int, background_task: BackgroundTasks):
     try:
         stock = session.query(Stock).get(id)
-        item = {}
-        item['stock'] = stock
-        item['stockInfo'] = session.query(StockInfo).filter(StockInfo.stockId == stock.id).first()
-        item['prices'] = session.query(Price).filter(Price.stockId == stock.id).order_by(desc(Price.date)).limit(31).all()
-        background_task.add_task(updateInfo, id)
-        background_task.add_task(updatePrices, id)
-        return item
+        if stock:
+            item = {}
+            item['stock'] = stock
+            item['stockInfo'] = session.query(StockInfo).filter(StockInfo.stockId == stock.id).first()
+            item['prices'] = session.query(Price).filter(Price.stockId == stock.id).order_by(desc(Price.date)).limit(31).all()
+            background_task.add_task(updateInfo, id)
+            background_task.add_task(updatePrices, id)
+            return item
+        else:
+            raise HTTPException(status_code=404, detail="Stock was not found!")
     except Exception as error:
-        return error
+        raise HTTPException(status_code=500, detail="Something went wrong!")
 
 @app.get('/api/stock/symbol/{symbol}')
 def getStockBySymbol(symbol: str, background_task: BackgroundTasks):
     try:
         stock = session.query(Stock).filter(func.lower(Stock.symbol) == func.lower(symbol)).first()
-        item = {}
-        item['stock'] = stock
-        item['stockInfo'] = session.query(StockInfo).filter(StockInfo.stockId == stock.id).first()
-        item['prices'] = session.query(Price).filter(Price.stockId == stock.id).order_by(desc(Price.date)).limit(31).all()
-        background_task.add_task(updateInfo, stock.id)
-        background_task.add_task(updatePrices, stock.id)
-        return item
+        if stock:
+            item = {}
+            item['stock'] = stock
+            item['stockInfo'] = session.query(StockInfo).filter(StockInfo.stockId == stock.id).first()
+            item['prices'] = session.query(Price).filter(Price.stockId == stock.id).order_by(desc(Price.date)).limit(31).all()
+            background_task.add_task(updateInfo, stock.id)
+            background_task.add_task(updatePrices, stock.id)
+            return item
+        else:
+            raise HTTPException(status_code=404, detail="Stock was not found!")
     except Exception as error:
-        return error
+        raise HTTPException(status_code=500, detail="Something went wrong!")
 
 @app.get('/api/stocks/exchange/{exchange}')
 def getStocksByExchange(exchange: str):
     try:
         stocks = session.query(Stock).filter(func.lower(Stock.exchange) == func.lower(exchange)).all()
-        result = []
-        for stock in stocks:
-            item = {}
-            item['stock'] = stock
-            item['stockInfo'] = session.query(StockInfo).filter(StockInfo.stockId == stock.id).first()
-            result.append(item)
-        return result
+        if stocks:
+            result = []
+            for stock in stocks:
+                item = {}
+                item['stock'] = stock
+                item['stockInfo'] = session.query(StockInfo).filter(StockInfo.stockId == stock.id).first()
+                result.append(item)
+            return result
+        else:
+            raise HTTPException(status_code=404, detail="No stocks were found!")
     except Exception as error:
-        print(error)
+        raise HTTPException(status_code=500, detail="Something went wrong!")
 
 @app.get('/api/stocks/sector/{sector}')
 def getStocksBySector(sector: str):
     try:
         stocks = session.query(Stock).filter(func.lower(Stock.sector) == func.lower(sector)).all()
-        result = []
-        for stock in stocks:
-            item = {}
-            item['stock'] = stock
-            item['stockInfo'] = session.query(StockInfo).filter(StockInfo.stockId == stock.id).first()
-            result.append(item)
-        return result
+        if stocks:
+            result = []
+            for stock in stocks:
+                item = {}
+                item['stock'] = stock
+                item['stockInfo'] = session.query(StockInfo).filter(StockInfo.stockId == stock.id).first()
+                result.append(item)
+            return result
+        else:
+            raise HTTPException(status_code=404, detail="No stocks were found!")
     except Exception as error:
-        print(error)
-
+        raise HTTPException(status_code=500, detail="Something went wrong!")
+    
 @app.get('/api/stocks/industry/{industry}')
 def getStocksByIndustry(industry: str):
     try:
         stocks = session.query(Stock).filter(func.lower(Stock.industry) == func.lower(industry)).all()
-        result = []
-        for stock in stocks:
-            item = {}
-            item['stock'] = stock
-            item['stockInfo'] = session.query(StockInfo).filter(StockInfo.stockId == stock.id).first()
-            result.append(item)
-        return result
+        if stocks:
+            result = []
+            for stock in stocks:
+                item = {}
+                item['stock'] = stock
+                item['stockInfo'] = session.query(StockInfo).filter(StockInfo.stockId == stock.id).first()
+                result.append(item)
+            return result
+        else:
+            raise HTTPException(status_code=404, detail="No stocks were found!")
     except Exception as error:
-        print(error)
+        raise HTTPException(status_code=500, detail="Something went wrong!")

8f72794e78c0754b107a9f288a12149b43475343
Jayden Wright <jaydenkwright3@gmail.com>
2021-01-21 18:17:27 -0500

    Created react project

---
+++ client/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Stock from './components/Stock/Stock'
+
+function App() {
+  return (
+    <div className="App">
+      <Stock />
+    </div>
+  );
+}
+
+export default App;
